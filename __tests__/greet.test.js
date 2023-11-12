dbf28b6c1b2e70eec6a29b73bd4fb6cacafc6bda
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-11-12 15:33:15 +0100

    add: manage case 2 when name is undefined

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,13 +1,19 @@
-const greet = require('../greet')
+const greet = require('../greet');
 
 // Case 1
-test('greet should return Hello, Bob!', () => {
-  const result = greet('Bob')
-  expect(result).toBe('Hello, Bob!')
+test('greet should return Hello, Bob.', () => {
+  const result = greet('Bob');
+  expect(result).toBe('Hello, Bob.');
 });
 
 // Case 2 when name is null
-test('greet should return Hello, my friend! ', () => {
+test('greet should return Hello, my friend. ', () => {
   const result = greet(null);
-  expect(result).toBe('Hello, my friend!')
+  expect(result).toBe('Hello, my friend.');
+});
+
+// Case 2 when name is undefined
+test('greet should return Hello, my friend!', () => {
+  const result = greet(undefined);
+  expect(result).toBe('Hello, my friend.');
 });
--- greet.js
+++ greet.js
@@ -1,8 +1,8 @@
 console.log('Hello World')
 
 function greet (name) {
-  if(name == null) return "Hello, my friend!";
-  return `Hello, ${name}!`
+  if(name == null) return "Hello, my friend.";
+  return `Hello, ${name}.`
 }
 
 module.exports = greet
