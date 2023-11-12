ecf23e0efda7e35c7f659fd2de503b53f1e88b0e
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-11-12 15:28:43 +0100

    add: manage case 2 when name is null

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,6 +1,13 @@
 const greet = require('../greet')
 
-test('should return Hello, Bob!', () => {
+// Case 1
+test('greet should return Hello, Bob!', () => {
   const result = greet('Bob')
   expect(result).toBe('Hello, Bob!')
-})
+});
+
+// Case 2 when name is null
+test('greet should return Hello, my friend! ', () => {
+  const result = greet(null);
+  expect(result).toBe('Hello, my friend!')
+});
--- greet.js
+++ greet.js
@@ -1,6 +1,7 @@
 console.log('Hello World')
 
 function greet (name) {
+  if(name == null) return "Hello, my friend!";
   return `Hello, ${name}!`
 }
 
