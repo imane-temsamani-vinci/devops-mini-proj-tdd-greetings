24d3eba13e3e11480dae28723de92443e51af532
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-11-15 00:48:52 +0100

    refactor: add function greetMultipleNames

--- greet.js
+++ greet.js
@@ -1,7 +1,6 @@
 function greet (name) {
   if (Array.isArray(name)) {
-    if (name.length > 2) return `Hello, ${name.slice(0, -1).join(', ')} and ${name[name.length - 1]}.`
-    return `Hello, ${name[0]} and ${name[1]}.`
+    return greetMultipleNames(name)
   }
   if (name === null || name === undefined || name.trim() === '') return 'Hello, my friend.'
   if (isUpperCase(name)) return `HELLO, ${name}!`
@@ -11,6 +10,12 @@ function greet (name) {
 
 function isUpperCase (name) {
   return name === name.toUpperCase()
+}
+
+function greetMultipleNames (array) {
+  const lastName = array[array.length - 1]
+  const withoutLast = array.slice(0, -1)
+  return `Hello, ${withoutLast.join(', ')} and ${lastName}.`
 }
 
 module.exports = greet
