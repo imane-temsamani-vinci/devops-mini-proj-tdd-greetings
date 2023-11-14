1bad1eccdca21180e9f00e0b09c192474013dc90
Rekaya Nour <nourrkaya@live.be>
2023-11-14 21:11:09 +0100

    add: manage case 4 when name is an array of 2 names

--- greet.js
+++ greet.js
@@ -1,6 +1,8 @@
 function greet (name) {
+  if (Array.isArray(name)) return `Hello, ${name[0]} and ${name[1]}.`
   if (name === null || name === undefined || name.trim() === '') return 'Hello, my friend.'
   if (isUpperCase(name)) return `HELLO, ${name}!`
+
   return `Hello, ${name}.`
 }
 
