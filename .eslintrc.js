4eb1df6189410bde105a07e8e037132e3e0c5592
youssef-ouftallah-vinci <youssef.ouftallah@student.vinci.be>
2023-12-12 21:32:47 +0100

    update: format and lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,12 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: 'standard',
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-    semi: ['error', 'always']
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {},
 };
